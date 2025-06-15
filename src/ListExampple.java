02758639a973e58a2ec817009564a4eb9241493e
toktobek-01 <toktobek.d@gmail.com>
2025-06-14 21:40:20 -0500

    week 6

---
+++ src/ListExampple.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class ListExampple {
+    public static void main(String[] args) {
+        List<String> fruits = new ArrayList<>();
+
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Orange");
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+        fruits.remove("Apple");
+        System.out.println("Apple is remove: " + fruits);
+        System.out.println("Second fruit: " + fruits.get(1));
+        fruits.get(0);
+        System.out.println("After removing the first fruit we have: " + fruits);
+
+    }
+
+}

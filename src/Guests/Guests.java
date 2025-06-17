43f438760a359b7aa2751f79f654a64f67d9b44a
toktobek-01 <toktobek.d@gmail.com>
2025-06-16 22:40:35 -0600

    week 6

---
+++ src/Guests/Guests.java
@@ -0,0 +1,13 @@
+package Guests;
+import java.util.*;
+
+public class Guests {
+    public static void main(String[] args) {
+        List <String> names = new ArrayList<>();
+         names.add("Tom");
+         names.add("Jerry");
+         names.add("Spike");
+
+        System.out.println(names);
+    }
+}

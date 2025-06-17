80560366ccb91363d5c51b39891c4353b93afef5
toktobek-01 <toktobek.d@gmail.com>
2025-06-16 22:50:34 -0600

    week 6

--- src/Guests/Guests.java
+++ src/Guests/Guests.java
@@ -3,11 +3,24 @@ import java.util.*;
 
 public class Guests {
     public static void main(String[] args) {
-        List <String> names = new ArrayList<>();
-         names.add("Tom");
-         names.add("Jerry");
-         names.add("Spike");
+        List<String> names = new ArrayList<>();
+        names.add("Tom");
+        names.add("Jerry");
+        names.add("Spike");
 
         System.out.println(names);
+
+        names.add(1, "Tyke");
+
+        names.remove("Spike");
+
+        System.out.println("Updated names: " + names);
+
+        if (names.contains("Jerry")) {
+            System.out.println("Jerry is on the list.");
+        } else {
+            System.out.println("Jerry is not on the list.");
+        }
+
     }
 }
